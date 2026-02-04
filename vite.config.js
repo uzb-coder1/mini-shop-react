e151fa7f4f9ff5f0958cde9283695fbbbcf15c2e
Kichkintoy <xojimurodovjaloliddin7@gmail.com>
2026-02-04 16:38:01 +0500

    Yangi toza boshlanish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mini-shop-v2/', // GitHub-dagi yangi repository nomi bilan bir xil bo'lishi shart!
 })
