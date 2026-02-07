6bdfc0f997f471d8c2bcb4edd81eee6c55674761
Kichkintoy <xojimurodovjaloliddin7@gmail.com>
2026-02-07 18:31:57 +0500

    message

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mini-shop-v2/', // GitHub-dagi yangi repository nomi bilan bir xil bo'lishi shart!
 })
